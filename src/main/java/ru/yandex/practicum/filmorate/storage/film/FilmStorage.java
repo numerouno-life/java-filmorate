6332290fd290f4febe26b5d93e37de90b3a235eb
Numerouno_life <dollari.91@mail.ru>
2024-09-13 06:49:00 +0400

    reworked-TS-number 11 Не проходят тесты новой логики в Postman.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,11 +6,19 @@ import java.util.Collection;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film update(Film film);
+
     Collection<Film> getAllFilms();
+
     Film getFilmById(Long id);
+
     void addLike(Long id, Long userId);
+
     void removeLike(Long id, Long userId);
+
     List<Film> getMostPopularFilms(int count);
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/film/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/UserStorage.java
@@ -8,11 +8,19 @@ import java.util.List;
 public interface UserStorage {
 
     User create(User user);
+
     User update(User user);
+
     Collection<User> getAllUser();
+
     User getUserById(Long id);
+
     void addFriend(Long userId, Long friendId);
+
     void removeFriend(Long userId, Long friendId);
+
     List<User> getFriends(Long userId);
+
     List<User> getCommonFriends(Long userId, Long otherId);
+
 }
